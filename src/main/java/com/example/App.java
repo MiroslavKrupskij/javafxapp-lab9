1ecfa2e51a951343a92506f331bae97a1a09e619
MiroslavKrupskij <miroslavkrupskij@gmail.com>
2024-11-26 12:41:54 +0200

    Third commit/task3

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,34 +1,21 @@
 package com.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        TextField textField = new TextField();
-        textField.setPromptText("Type your message here:");
-
-        Button printButton = new Button("Print message");
-        printButton.setOnAction(e -> {
-            String inputText = textField.getText();
-            System.out.println("Your message: " + inputText);
-        });
-
-        Button clearButton = new Button("Clear");
-        clearButton.setOnAction(e -> textField.clear());
-
-        VBox root = new VBox(10);
-        root.getChildren().addAll(textField, printButton, clearButton);
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/sample.fxml"));
+        Parent root = loader.load();
 
         Scene scene = new Scene(root, 300, 200);
 
-        primaryStage.setTitle("Розширене JavaFX-застосування");
+        primaryStage.setTitle("FXML JavaFX Application");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
